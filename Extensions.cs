3cfd04b74597ba0605e0ca252947828ee310e9aa
shrikant mahajan <shrikanttechwork@hotmail.com>
2021-08-31 14:12:43 +0530

    Add project files.

---
+++ Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Play.Catalog.Service.Dtos;
+using Play.Catalog.Service.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Play.Catalog.Service
+{
+	public static class Extensions
+	{
+		public static ItemDto AsDto(this Item item)
+		{
+			return new ItemDto(item.Id, item.Name, item.Description, item.Price, item.CreatedDate);
+		}
+	}
+}
---
+++ Settings/MongoDbSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Play.Catalog.Service.Settings
+{
+	public class MongoDbSettings
+	{
+		public string Host { get; init; }
+		public string Port { get; init; }
+		public string ConnectionString => $"mongodb://{Host}:{Port}";
+	}
+}
